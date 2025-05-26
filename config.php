059ad9ee5b0c9cc7f0baa5a4720e290c1123640d
GionLennon <148975668+GionLennon@users.noreply.github.com>
2025-05-26 21:22:11 +0200

    Delete config.php

--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$pdo = new PDO("mysql:host=localhost;dbname=YOUR_DB", "YOUR_USER", "YOUR_PASS", [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
-?>
